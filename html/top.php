11938ae176b6718bea698b432db902fda9cc8468
fl-kento <kento.matsumoto@rakumachi.co.jp>
2024-11-21 11:03:43 +0900

    ログイン画面の実装（再修正+リダイレクト）

--- html/top.php
+++ html/top.php
@@ -1,22 +1,15 @@
 <?php
 $errorMessage = [];
-if (!empty($_POST["login"])) {
-  if (empty($_POST["name"])) {
-    $errorMessage["name"] = "ユーザ名を入力してください";
+if (!empty($_POST['login'])) {
+  if (empty($_POST['name'])) {
+    $errorMessage['name'] = 'ユーザ名を入力してください';
   }
-  elseif (mb_strlen($_POST["name"])>20) {
-    $errorMessage["name"] = "文字数がオーバーしています";
+  if (empty($_POST['password'])) {
+    $errorMessage['password'] = 'パスワードを入力してください';
   }
-  if (empty($_POST["password"])) {
-    $errorMessage["password"] = "パスワードを入力してください";
-  }
-  else {
-    if (!preg_match(" /^[a-zA-Z0-9]+$/",$_POST["password"])) {
-      $errorMessage["password"] = "大文字、小文字、数値で入力してください";
-    }
-    if (mb_strlen($_POST["password"])<10) {
-      $errorMessage["password"] = "10桁以上で入力してください";
-    }
+  if (empty($errorMessage)) {
+    header('Location: expense/expense.php');
+    exit();
   }
 }
 ?>
@@ -34,21 +27,17 @@ if (!empty($_POST["login"])) {
     <h1 class="title">家計簿アプリ</h1>
     <form action="top.php" method="post">
       <p>ユーザー名: <input type="text" name="name"></p>
-      <p class="error">
-        <?php
-          if (!empty($errorMessage["name"])) {
-            echo $errorMessage["name"];
-          } 
-        ?>
-      </p>
+      <p class="error"><?php
+        if (!empty($errorMessage['name'])) {
+          echo $errorMessage['name'];
+        } 
+      ?></p>
       <p>パスワード: <input type="password" name="password"></p>
-      <p class="error">
-        <?php
-          if (!empty($errorMessage["password"])) {
-              echo $errorMessage["password"];
-          }
-        ?>
-      </p>
+      <p class="error"><?php
+        if (!empty($errorMessage['password'])) {
+          echo $errorMessage['password'];
+        }
+      ?></p>
       <input type="submit" name="login" value="ログイン">
     </form>
     <div class="button"><a href="register.html" class="register">新規登録</a></div>
