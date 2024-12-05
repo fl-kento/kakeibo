8d0671a8d8bf1530b956a2e787f8a5404979082c
fl-kento <kento.matsumoto@rakumachi.co.jp>
2024-12-05 18:13:49 +0900

    収入関連画面の実装

--- html/income/income.php
+++ html/income/income.php
@@ -1,3 +1,29 @@
+<?php 
+session_start();
+try {
+  $db = new PDO('mysql:dbname=money_management;host=127.0.0.1;charset=utf8', 'root', '');
+} catch (PDOException $e) {
+  echo 'DB接続エラー:' . $e->getMessage();
+}
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+  $_SESSION['time'] = time();
+  $content = $db->prepare('SELECT content, amount, income_no FROM income WHERE user_id = :id');
+  $content->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $content->execute();
+  $user = $db->prepare('SELECT name FROM user WHERE id = :id');
+  $user->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $user->execute();
+  $user = $user->fetch(PDO::FETCH_ASSOC);
+  $user_name = $user['name'];
+  $sum = $db->prepare('SELECT SUM(amount) AS 合計金額 FROM income WHERE user_id = :id');
+  $sum->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $sum->execute();
+  $sum_result = $sum->fetch(PDO::FETCH_ASSOC);
+} else {
+  header('Location: ../top.php');
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,44 +31,44 @@
   <meta name="keywords" content="家計簿アプリ,使いやすい,household account book">
   <meta name="description" content="家計簿アプリです">
   <link rel="stylesheet" href="../../css/main.css">
-  <link rel="stylesheet" href="../../css/income/income.css">
-  <title>Document</title>
+  <link rel="stylesheet" href="../../css/manage.css">
+  <title>収入管理画面</title>
 </head>
 <body>
   <div class="main">
-    <div class="sidebar">
-      <div class="username"><span>研人</span>さん</div>
-      <div class="sidebar_content payment"><a href="../expence/expense.html">支出管理</a></div>
-      <div class="sidebar_content income"><a href="income.html">収入管理</a></div>
-      <div class="sidebar_content fixedcosts"><a href="../fc/fc.html">固定費管理</a></div>
-      <div class="sidebar_content logout"><a href="../top.html">ログアウト</a></div>
+  <div class="sidebar">
+      <div class="sidebar_content"><span class="username"><?php echo $user_name; ?></span>さん</div>
+      <div class="sidebar_content"><a href="../expense/expense.php">支出管理</a></div>
+      <div class="sidebar_content income"><a href="income.php">収入管理</a></div>
+      <div class="sidebar_content"><a href="../fc/fc.php">固定費管理</a></div>
+      <div class="sidebar_content logout">
+        <a href="../../logout.php">ログアウト</a>
+      </div>
     </div>
     <div class="content">
       <h1>10月の収入</h1>
-      <a class="btn_add" href="income_add.html">収入の追加</a>
-      <table>
-        <tr>
-          <td class="sum">合計</td>
-          <td class="sum" colspan="2">300,000</td>
-        </tr>
-        <tr>
-          <td>給料</td>
-          <td>100,000</td>
-          <td><a href="income_edit.html">編集・削除</a></td>
-        </tr>
-        <tr>
-          <td>競馬</td>
-          <td>5,000</td>
-          <td><a href="income_edit.html">編集・削除</a></td>
-        </tr>
-        <tr>
-          <td>宝くじ</td>
-          <td>10,000</td>
-          <td><a href="income_edit.html">編集・削除</a></td>
-        </tr>
-      </table>
+      <a class="btn_add" href="income_add.php">収入の追加</a>
+      <p class="sum">合計:<?php
+        if (empty($sum_result['合計金額'])) {
+          echo '0';
+        } else {
+          echo $sum_result['合計金額']; 
+        }
+      ?>円</p>
+      <div class="table_box">
+        <table>
+        <?php 
+        foreach ($content->fetchAll() as $result): 
+        ?>
+          <tr>
+            <td><?php echo $result['content']; ?></td>
+            <td><?php echo $result['amount']; ?> 円</td>
+            <td><a href="income_edit.php?no=<?php print($result['income_no']); ?>">編集・削除</a></td>          
+          </tr>
+        <?php endforeach; ?> 
+        </table>
+      </div>
     </div>
   </div>
-
 </body>
 </html>
--- html/income/income_add.php
+++ html/income/income_add.php
@@ -1,3 +1,60 @@
+<?php
+session_start();
+try {
+  $db = new PDO('mysql:dbname=money_management;host=127.0.0.1;charset=utf8', 'root', '');
+} catch (PDOException $e) {
+  echo 'DB接続エラー:' . $e->getMessage();
+}
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+  $_SESSION['time'] = time();
+  $user = $db->prepare('SELECT name FROM user WHERE id = :id');
+  $user->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $user->execute();
+  $user = $user->fetch(PDO::FETCH_ASSOC);
+  $user_name = $user['name'];
+  $latest_no = $db->prepare('SELECT income_no FROM income WHERE user_id = :id ORDER BY income_no DESC LIMIT 1');
+  $latest_no->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $latest_no->execute();
+  $latest_no = $latest_no->fetch(PDO::FETCH_ASSOC);
+  if (empty($latest_no['income_no'])) {
+    $latest_no['income_no'] = 1;
+  } else {
+    $latest_no['income_no'] += 1;
+  }
+  $error_message = [];
+  if (!empty($_POST['add'])) {
+    if (empty($_POST['content'])) {
+      $error_message['content'] = '内容を入力してください';
+    }
+    if (empty($_POST['money'])) {
+      $error_message['money'] = '金額を入力してください';
+    }
+    if (empty($_POST['date'])) {
+      $error_message['date'] = '日付を選んでください';
+    }
+    if (strlen($_POST['money']) > 6) {
+      $error_message['big'] = "金額が大きすぎます";
+    }
+    if (mb_strlen($_POST['content']) > 16) {
+      $error_message['length'] = "内容が長すぎます";
+    }
+    if (empty($error_message)) {
+      $add = $db->prepare('INSERT INTO income (income_no, user_id, content, amount, date) VALUE (:number, :id, :content, :money, :date)');
+      $add->bindParam(':number', $latest_no['income_no'], PDO::PARAM_INT);
+      $add->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+      $add->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
+      $add->bindParam(':money', $_POST['money'], PDO::PARAM_INT);    
+      $add->bindParam(':date', $_POST['date'], PDO::PARAM_STR);    
+      $add->execute();
+      header('Location: income.php');
+      exit();
+    }
+  }
+} else {
+  header('Location: ../top.php');
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,24 +62,31 @@
   <meta name="keywords" content="家計簿アプリ,使いやすい,household account book">
   <meta name="description" content="家計簿アプリです">
   <link rel="stylesheet" href="../../css/main.css">
-  <link rel="stylesheet" href="../../css/income/income_add.css">
-  <title>Document</title>
+  <link rel="stylesheet" href="../../css/add.css">
+  <title>収入追加画面</title>
 </head>
 <body>
   <div class="main">
     <div class="sidebar">
-      <div class="username"><span>研人</span>さん</div>
-      <div class="sidebar_content payment"><a href="../expence/expense.html">支出管理</a></div>
-      <div class="sidebar_content income"><a href="income.html">収入管理</a></div>
-      <div class="sidebar_content fixedcosts"><a href="../fc/fc.html">固定費管理</a></div>
-      <div class="sidebar_content logout"><a href="../top.html">ログアウト</a></div>
+      <div class="sidebar_content"><span class="username"><?php echo $user_name; ?></span>さん</div>
+      <div class="sidebar_content"><a href="../expense/expense.php">支出管理</a></div>
+      <div class="sidebar_content income"><a href="income.php">収入管理</a></div>
+      <div class="sidebar_content"><a href="../fc/fc.php">固定費管理</a></div>
+      <div class="sidebar_content">
+        <a href="../../logout.php">ログアウト</a>
+      </div>
     </div>
     <div class="content">
       <h1 class="title">収入の追加</h1>
-      <form action="income.html" method="post">
-        <p>内容: <input type="text" name="name"></p>
+      <form action="income_add.php" method="post">
+        <h3><?php
+          foreach ($error_message as $message) {
+            echo $message . '<br>';
+          } 
+        ?></h3>
+        <p>内容: <input type="text" name="content"></p>
         <p>金額: <input type="text" name="money"> 円</p>
-        <p>日付: <input type="date" name="date"></p>
+        <p>日付: <input type="date" name="date" value="<?php echo date("Y-m-d"); ?>"></p>
         <p class="decision"><input type="submit" name="add" value="追加"></p>
       </form>
     </div>
