aac77cabf9d85f93633334930755d833feff1905
fl-kento <kento.matsumoto@rakumachi.co.jp>
2024-12-12 13:45:12 +0900

    固定費関連画面の追加

--- html/fc/fc.php
+++ html/fc/fc.php
@@ -1,3 +1,29 @@
+<?php 
+session_start();
+try {
+  $db = new PDO('mysql:dbname=money_management;host=127.0.0.1;charset=utf8', 'root', '');
+} catch (PDOException $e) {
+  echo 'DB接続エラー:' . $e->getMessage();
+}
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+  $_SESSION['time'] = time();
+  $content = $db->prepare('SELECT content, amount, payment_date, fixed_no FROM fixed WHERE user_id = :id');
+  $content->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $content->execute();
+  $user = $db->prepare('SELECT name FROM user WHERE id = :id');
+  $user->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $user->execute();
+  $user = $user->fetch(PDO::FETCH_ASSOC);
+  $user_name = $user['name'];
+  $sum = $db->prepare('SELECT SUM(amount) AS 合計金額 FROM fixed WHERE user_id = :id');
+  $sum->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $sum->execute();
+  $sum_result = $sum->fetch(PDO::FETCH_ASSOC);
+} else {
+  header('Location: ../top.php');
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,49 +31,53 @@
   <meta name="keywords" content="家計簿アプリ,使いやすい,household account book">
   <meta name="description" content="家計簿アプリです">
   <link rel="stylesheet" href="../../css/main.css">
-  <link rel="stylesheet" href="../../css/fc/fc.css">
-  <title>Document</title>
+  <link rel="stylesheet" href="../../css/manage.css">
+  <title>固定費管理画面</title>
 </head>
 <body>
   <div class="main">
     <div class="sidebar">
-      <div class="username"><span>研人</span>さん</div>
-      <div class="sidebar_content expence"><a href="../expence/expense.html">支出管理</a></div>
-      <div class="sidebar_content income"><a href="../income/income.html">収入管理</a></div>
-      <div class="sidebar_content fixedcosts"><a href="fc.html">固定費管理</a></div>
-      <div class="sidebar_content logout"><a href="../top.html">ログアウト</a></div>
+      <div class="sidebar_content"><span class="username"><?php echo $user_name; ?></span>さん</div>
+      <div class="sidebar_content"><a href="../expense/expense.php">支出管理</a></div>
+      <div class="sidebar_content"><a href="../income/income.php">収入管理</a></div>
+      <div class="sidebar_content fixedcosts"><a href="fc.php">固定費管理</a></div>
+      <div class="sidebar_content logout">
+        <a href="../../logout.php">ログアウト</a>
+      </div>
     </div>
     <div class="content">
-      <h1>合計金額 : 14,500</h1>
-      <a class="btn_add" href="fc_add.html">固定費の追加</a>
-      <table>
-        <tr>
-          <th>内容</th>
-          <th>金額</th>
-          <th>支払日</th>
-          <th>編集・削除</th>
-        </tr>
-        <tr>
-          <td>保険料</td>
-          <td>10,000</td>
-          <td>1日</td>
-          <td><a href="fc_edit.html">編集・削除</a></td>
-        </tr>
-        <tr>
-          <td>楽待プレミアム</td>
-          <td>3,300</td>
-          <td>23日</td>
-          <td><a href="fc_edit.html">編集・削除</a></td>
-        </tr>
-        <tr>
-          <td>amapura</td>
-          <td>1,200</td>
-          <td>11日</td>
-          <td><a href="fc_edit.html">編集・削除</a></td>
-        </tr>
-      </table>
+      <h1>合計金額:<?php
+        if (empty($sum_result['合計金額'])) {
+          echo '0';
+        } else {
+          echo $sum_result['合計金額']; 
+        }
+      ?>円</h1>
+      <a class="btn_add" href="fc_add.php">固定費の追加</a>
+      <div class="table_box">
+        <table>
+          <tr>
+            <th>内容</th>
+            <th>金額</th>
+            <th>支払日</th>
+            <th>編集・削除</th>
+          </tr>
+          <?php foreach ($content->fetchAll() as $result): ?>
+          <tr>
+            <td><?php echo $result['content']; ?></td>
+            <td><?php echo $result['amount']; ?> 円</td>
+            <td><?php echo $result['payment_date']; ?> 日</td>
+            <td><a href="fc_edit.php?no=<?php print($result['fixed_no']); ?>">編集・削除</a></td>          
+          </tr>
+          <?php endforeach; ?>
+          <?php if (empty($result)): ?>
+          <tr>
+            <td colspan="4">なし</td>
+          </tr>
+          <?php endif; ?>
+        </table>
+      </div>
     </div>
   </div>
-
 </body>
 </html>
--- html/fc/fc_edit.php
+++ html/fc/fc_edit.php
@@ -1,3 +1,69 @@
+<?php
+session_start();
+try {
+  $db = new PDO('mysql:dbname=money_management;host=127.0.0.1;charset=utf8', 'root', '');
+} catch (PDOException $e) {
+  echo 'DB接続エラー:' . $e->getMessage();
+}
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+  $_SESSION['time'] = time();
+  $user = $db->prepare('SELECT name FROM user WHERE id = :id');
+  $user->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $user->execute();
+  $user = $user->fetch(PDO::FETCH_ASSOC);
+  $user_name = $user['name'];
+  $now_content = $db->prepare('SELECT content, amount, payment_date FROM fixed WHERE user_id = :id AND fixed_no = :no');
+  $now_content->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+  $now_content->bindParam(':no', $_REQUEST['no'], PDO::PARAM_INT);
+  $now_content->execute();
+  $now_content = $now_content->fetch(PDO::FETCH_ASSOC);
+  $error_message = [];
+  $content = $now_content['content'];
+  $money = $now_content['amount'];
+  $date = $now_content['payment_date'];
+  if (!empty($_POST['edit'])) {
+    if (empty($_POST['content'])) {
+      $error_message['content'] = '内容を入力してください';
+    }
+    if (empty($_POST['money'])) {
+      $error_message['money'] = '金額を入力してください';
+    }
+    if (empty($_POST['date'])) {
+      $error_message['date'] = '支払日を入力してください';
+    } elseif (1 > $_POST['date'] or $_POST['date'] > 31) {
+      $error_message['Incorrect_format'] = "正しい日にちを入力してください";
+    }
+    if (strlen($_POST['money']) > 6) {
+      $error_message['big'] = "金額が大きすぎます";
+    }
+    if (mb_strlen($_POST['content']) > 20) {
+      $error_message['length'] = "内容が長すぎます";
+    }
+    if (empty($error_message)) {
+      $edit = $db->prepare('UPDATE fixed SET content = :content, amount = :money, payment_date = :date WHERE user_id = :id AND fixed_no = :no');
+      $edit->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+      $edit->bindParam(':no', $_REQUEST['no'], PDO::PARAM_INT);
+      $edit->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
+      $edit->bindParam(':money', $_POST['money'], PDO::PARAM_INT);
+      $edit->bindParam(':date', $_POST['date'], PDO::PARAM_STR);
+      $edit->execute();
+      header('Location: fc.php');
+      exit();
+    }
+  }
+  if (!empty($_POST['delete'])) {
+    $delete = $db->prepare('DELETE FROM fixed WHERE user_id = :id AND fixed_no = :no');
+    $delete->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
+    $delete->bindParam(':no', $_REQUEST['no'], PDO::PARAM_INT);
+    $delete->execute();
+    header('Location: fc.php');
+    exit();
+  }
+} else {
+  header('Location: ../top.php');
+  exit();  
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,34 +71,37 @@
   <meta name="keywords" content="家計簿アプリ,使いやすい,household account book">
   <meta name="description" content="家計簿アプリです">
   <link rel="stylesheet" href="../../css/main.css">
-  <link rel="stylesheet" href="../../css/fc/fc_edit.css">
-  <title>Document</title>
+  <link rel="stylesheet" href="../../css/edit.css">
+  <title>固定費編集画面</title>
 </head>
 <body>
   <div class="main">
     <div class="sidebar">
-      <div class="username"><span>研人</span>さん</div>
-      <div class="sidebar_content payment"><a href="../expence/expense.html">支出管理</a></div>
-      <div class="sidebar_content income"><a href="../income/income.html">収入管理</a></div>
-      <div class="sidebar_content fixedcosts"><a href="fc.html">固定費管理</a></div>
-      <div class="sidebar_content logout"><a href="../top.html">ログアウト</a></div>
+      <div class="sidebar_content"><span class="username"><?php echo $user_name; ?></span>さん</div>
+      <div class="sidebar_content"><a href="../expense/expense.php">支出管理</a></div>
+      <div class="sidebar_content"><a href="../income/income.php">収入管理</a></div>
+      <div class="sidebar_content fixedcosts"><a href="fc.php">固定費管理</a></div>
+      <div class="sidebar_content logout">
+        <a href="../../logout.php">ログアウト</a>
+      </div>
     </div>
     <div class="content">
       <h1 class="title">固定費項目の編集・削除</h1>
-      <form action="fc.html" method="post">
-        <p>内容: <input type="text" name="name"></p>
-        <p class="now_value">現在の内容: 楽待プレミアム</p>
-        <p>金額: <input type="text" name="money"> 円</p>
-        <p class="now_value">現在の金額: 3,300</p>
-        <p>日付: <input type="number" name="date" class="number"></p>
-        <p class="now_value">現在の支払日: 25日</p>
+      <form action="fc_edit.php?no=<?php print($_REQUEST['no']); ?>" method="post">
+        <h3><?php
+          foreach ($error_message as $message) {
+            echo $message . '<br>';
+          } 
+        ?></h3>
+        <p class="item">内容: <input type="text" name="content" value="<?php echo $content; ?>"></p>
+        <p class="item">金額: <input type="text" name="money" value="<?php echo $money; ?>"> 円</p>
+        <p class="item">支払日: <input type="number" name="date" value="<?php echo $date; ?>"> 日</p>
         <p class="decision">
-          <input type="submit" name="add" value="編集">
+          <input type="submit" name="edit" value="編集">
           <input type="submit" name="delete" value="削除">
         </p>
       </form>
     </div>
   </div>
-
 </body>
 </html>
