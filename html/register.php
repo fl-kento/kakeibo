b422d37a7aabc4a3ed0082bf1e401de852130a33
fl-kento <kento.matsumoto@rakumachi.co.jp>
2024-12-17 11:45:34 +0900

    再修正

--- html/register.php
+++ html/register.php
@@ -4,34 +4,34 @@ try {
 } catch (PDOException $e) {
   echo 'DB接続エラー:' . $e->getMessage();
 }
-$errorMessage = [];
+$error_message = [];
 $latest_no = $db->prepare('SELECT id FROM user ORDER BY id DESC LIMIT 1');
 $latest_no->execute();
 $latest_no = $latest_no->fetch(PDO::FETCH_ASSOC);
 $latest_no['id'] += 1;
 if (!empty($_POST['login'])) {
   if (empty($_POST['name'])) {
-    $errorMessage['name'] = 'ユーザ名を入力してください';
+    $error_message['name'] = 'ユーザ名を入力してください';
   }
   elseif (mb_strlen($_POST['name']) > 20) {
-    $errorMessage['name'] = '文字数がオーバーしています';
+    $error_message['name'] = '文字数がオーバーしています';
   }
   if (empty($_POST['password'])) {
-    $errorMessage['password'] = 'パスワードを入力してください';
+    $error_message['password'] = 'パスワードを入力してください';
   }
   else {
     if (!preg_match(' /^[a-zA-Z0-9]+$/', $_POST['password'])) {
-      $errorMessage['password'] = '大文字、小文字、数値で入力してください';
+      $error_message['password'] = '大文字、小文字、数値で入力してください';
     }
     if (mb_strlen($_POST['password']) < 10) {
-      $errorMessage['password'] = '10桁以上で入力してください';
+      $error_message['password'] = '10桁以上で入力してください';
     }
   }
-  if (empty($errorMessage)) {
+  if (empty($error_message)) {
     $regist = $db->prepare('INSERT INTO user (id, name, password) VALUE (:id, :name, :password)');
     $regist->bindParam(':id', $latest_no['id'], PDO::PARAM_INT);
     $regist->bindParam(':name', $_POST['name'], PDO::PARAM_STR);
-    $regist->bindParam(':password', $_POST['password'], PDO::PARAM_INT);
+    $regist->bindParam(':password', $_POST['password'], PDO::PARAM_STR);
     $regist->execute();
     header('Location: completed.php');
     exit();
@@ -53,14 +53,14 @@ if (!empty($_POST['login'])) {
     <form action="register.php" method="post">
       <p>ユーザー名: <input type="text" name="name"></p>
       <p class="error"><?php
-        if (!empty($errorMessage['name'])) {
-          echo $errorMessage['name'];
+        if (!empty($error_message['name'])) {
+          echo $error_message['name'];
         } 
       ?></p>
       <p>パスワード: <input type="password" name="password"></p>
       <p class="error"><?php
-        if (!empty($errorMessage['password'])) {
-          echo $errorMessage['password'];
+        if (!empty($error_message['password'])) {
+          echo $error_message['password'];
         } 
       ?></p>
       <p class="decision"><input type="submit" name="login" value="新規登録"></p>
